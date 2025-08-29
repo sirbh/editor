0a4d5cf3746e849be5bd3d95a5f706d7031e1bb2
saurabh chauhan <sourabh1202@gmail.com>
2025-08-29 04:36:45 +0300

    added upload logic

--- src/components/sections/assets/Assets.tsx
+++ src/components/sections/assets/Assets.tsx
@@ -1,18 +1,49 @@
 import Card from "../../ui/card/Card";
 import Button from "../../ui/button/Button";
-import styles from "./Assets.module.css"
+import styles from "./Assets.module.css";
+import { useRef, useState, type ChangeEvent } from "react";
 
 export default function Assets() {
+  const [files, setFiles] = useState<File[]>([]);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
 
-    return (
-        <Card className={styles.container}>
-            <div className={styles['container-head']}>
-                <h4 className={styles.header}>
-                    Your assets
-                </h4>
-                <Button name="Upload Assets" className={styles.btn} />
-            </div>
-        </Card>
-    );
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const newFiles = Array.from(e.target.files);
+      setFiles((prev) => [...prev, ...newFiles]);
+      e.target.value = "";
+    }
+  };
+
+  const handleClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  return (
+    <Card className={styles.container}>
+      <div className={styles["container-head"]}>
+        <h4 className={styles.header}>Your assets</h4>
+
+        <input
+          ref={fileInputRef}
+          type="file"
+          id="upload-input"
+          accept="image/*,video/*"
+          multiple
+          style={{ display: "none" }}
+          onChange={handleChange}
+        />
+
+        <Button name="Upload Assets" className={styles.btn} onClick={handleClick} />
+      </div>
+
+      <div style={{overflow: "hidden"}}>
+        <ul style={{ marginTop: "15px" }}>
+          {files.map((file, index) => (
+            <li key={`${file.name}-${index}`}>{file.name}</li>
+          ))}
+        </ul>
+      </div>
+    </Card>
+  );
 }
-
--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -4,10 +4,11 @@ import styles from "./Button.module.css"
 interface ButtonProps {
    name:string;
    className?:string;
+   onClick?: () => void;    
 }
 
-export default function Button({name,className}:ButtonProps) {
-    return <button className={classNames(styles.button,className)}>
+export default function Button({name,className,onClick}:ButtonProps) {
+    return <button className={classNames(styles.button,className)} onClick={onClick}>
           {name}
     </button>
 }
