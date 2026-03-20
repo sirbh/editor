3487fcfdd12666538df31591a628b279b03b860a
saurabh chauhan <sourabh1202@gmail.com>
2026-03-21 00:47:12 +0200

    added seek bar with layers

--- src/components/custom/Seekbar.tsx
+++ src/components/custom/Seekbar.tsx
@@ -1,149 +1,169 @@
-// import { useState } from "react";
-// import type { ChangeEvent } from "react";
+import { Button } from "@/components/ui/button";
+import { Slider } from "@/components/ui/slider";
+import {
+  Play,
+  ChevronsLeft,
+  ChevronsRight,
+  ArrowLeftFromLine,
+} from "lucide-react";
+import { ScrollArea } from "../ui/scroll-area";
 
-import type { RootState } from "@/store/store";
-import { useDispatch, useSelector } from "react-redux";
-import { Rnd } from "react-rnd";
-import { Slider } from "../ui/slider";
-import { updateSelectedAssetRange } from "@/store/assets";
+import { SnapModifier, AxisModifier } from "@dnd-kit/abstract/modifiers";
+import { PointerActivationConstraints } from "@dnd-kit/dom";
+import { DragDropProvider, PointerSensor, useDraggable, useDroppable } from "@dnd-kit/react";
+import { useState } from "react";
+import type { DragEndEvent } from "@dnd-kit/core";
 
+type ItemDetail = {
+  width: number;
+  start: number; // <- here
+};
 
+type Item = {
+  id: string;
+  type: "text" | "image" | "video" | "audio";
+  detail: ItemDetail;
+};
 
-export default function Seekbar({time,setTime}:{
-  time:number,
-  setTime:(time:number)=>void
-}) {
-  // const [value, setValue] = useState<number>(50);
-  const duration_sec = 120
-  const duration_px = 750
-  const slot = (duration_px * 16.6) / (120 * 1000)
+type DataMap = Record<string, Item>;
 
+export default function Seekbar() {
+  const [data, setData] = useState<DataMap>({
+    a1b2c3d: {
+      id: "a1b2c3d",
+      type: "image",
+      detail: {
+        width: 180,
+        start: 45,
+      },
+    },
+    x9y8z7k: {
+      id: "x9y8z7k",
+      type: "audio",
+      detail: {
+        width: 250,
+        start: 120,
+      },
+    },
+  });
 
-  // const [time, setTime] = useState(0);
-  // const [playing, setPlaying] = useState(false);
-  const assets = useSelector((state: RootState) => state.assets.selectedAssests);
-  console.log(assets)
-  const dispatch = useDispatch();
+  console.log(data)
 
-  // useEffect(() => {
-  //   let interval: NodeJS.Timeout;
-  //   if (playing) {
-  //     interval = setInterval(() => {
-  //       setTime((t) => (t < duration_sec * 1000 ? t + 16.6 : t));
-  //     }, 16.6); // advance 1 sec per second
-  //   }
-  //   return () => clearInterval(interval);
-  // }, [playing]);
+  const { ref } = useDroppable({ id: 'droppable' });
+
+  const handleDragEnd = (e) => {
+    const id = e.operation.source?.id;
+    if (!id) return;
+    const { x } = e.operation.transform;
+
+    setData((prev) => {
+      const currentItem = prev[id];
+      if (!currentItem) return prev;
+
+      return {
+        ...prev,
+        [id]: {
+          ...currentItem,
+          detail: {
+            ...currentItem.detail,
+            // Add the movement (x) to the existing starting point
+            start: currentItem.detail.start + x,
+          },
+        },
+      };
+    });
+  };
+  return (
+    <DragDropProvider
+      modifiers={[
+        SnapModifier.configure({
+          size: {
+            x: 2,
+            y: 40,
+          },
+        }),
+      ]}
+      onDragEnd={handleDragEnd}
+    >
+      <div className="flex flex-col h-full">
+        <div className="relative p-6 h-[3rem] w-full !mt-4">
+          {/* CENTER - Play button */}
+          <div className="absolute left-1/2 -translate-x-1/2">
+            <Button className="w-10 h-10 rounded-full flex items-center justify-center">
+              <Play />
+            </Button>
+          </div>
+
+          {/* LEFT of center */}
+          <div className="absolute right-1/2 -translate-x-8 flex gap-2">
+            <Button
+              variant="ghost"
+              className="w-10 h-10 rounded-full flex items-center justify-center"
+            >
+              <ArrowLeftFromLine />
+            </Button>
+            <Button
+              variant="ghost"
+              className="w-10 h-10 rounded-full flex items-center justify-center"
+            >
+              <ChevronsLeft />
+            </Button>
+            <Button
+              variant="ghost"
+              className="w-10 h-10 rounded-full flex items-center justify-center"
+            >
+              <ChevronsRight />
+            </Button>
+          </div>
+
+          {/* RIGHT of center */}
+          <div className="absolute left-1/2 translate-x-12 h-10 flex items-center">
+            0:23/4:00
+          </div>
+        </div>
+        <div className="h-[1.5rem] bg-transparent">
+          <Slider />
+        </div>
+        <ScrollArea className="flex-1 min-h-0 !pr-4">
+          {Object.entries(data).map((ele) => {
+            return (
+              <div key={ele[0]} ref={ref} className="w-full bg-transparent rounded h-[2.5rem] relative !py-[2px]">
+                <div className="w-full bg-gray-200 rounded h-full">
+                  <Draggable
+                    id={ele[0]}
+                    left={ele[1].detail.start}
+                  >
+                    <div
+                      className="h-full bg-blue-200 rounded border border-blue-900"
+                      style={{ width: ele[1].detail.width }}
+                    ></div>
+                  </Draggable>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollArea>
+      </div>
+    </DragDropProvider>
+  );
+}
+
+function Draggable(props) {
+  const { ref } = useDraggable({
+    id: props.id,
+  });
 
 
   return (
-
-      <>
-
-      <Slider value={[time]} onValueChange={(e) => setTime(e[0])} className="h-5 w-[750px] " step={slot} min={0} max={duration_sec*1000} />
-
-        <div style={{ width: `${duration_px}px`, height: "240px", alignItems: "center", display: "flex", flexDirection: "column", justifyContent: "start", zIndex: 1, position: "relative", backgroundColor: "blue" }}>
-
-          {
-            assets.map(ele => {
-              console.log(ele.end,ele.start)
-              return <div style={{ width: '100%', height:'40px', position:'relative' }}>
-                <Rnd
-                  // onDrag={(e, data) => {
-                  //   console.log(data.x)
-                  // }}
-
-                  onDragStop={(e, data) => {
-                    const duration = ele.end-ele.start
-                    const start = (data.x)*(120000/750)
-                    const end = (start + duration);
-                    dispatch(updateSelectedAssetRange({
-                      id:ele.select_id,
-                      start,
-                      end
-                    }))
-                  }}
-                  default={{
-                    x: 0,
-                    y: 0,
-                    width: (ele.end - ele.start) * (750 / 120000),
-                    height: 40,
-                  }}
-                  enableResizing={false}
-                  bounds={"parent"}
-                  dragAxis="x"
-                  dragGrid={[slot, 5]}
-                >
-                  <div style={{ backgroundColor: "brown", height: '100%', width: '100%' }}>
-
-                  </div>
-                </Rnd>
-              </div>
-            })
-
-          }
-          {/* <div style={{ width: '100%' }}>
-          <Rnd
-            onDrag={(e, data)=>{
-                console.log(data.x)
-            }}
-            default={{
-              x: 0,
-              y: 0,
-              width: 320,
-              height: 40,
-            }}
-            enableResizing={false}
-            bounds={"parent"}
-            dragAxis="x"
-            dragGrid={[slot,5]}
-          >
-            <div style={{ backgroundColor:"brown", height:'100%', width:'100%'}}>
-
-            </div>
-          </Rnd>
-        </div>  */}
-
-        </div>
-      </>
-
+    <div
+      ref={ref}
+      className={`h-full absolute top-0 !py-[2px]`}
+      style={{ left: props.left }}
+    >
+      {props.children}
+    </div>
   );
-};
+}
 
 
 
-// function Draggable({ children, id, cordinates }: { children: React.ReactNode, id?: string, cordinates: { x: number, y: number } }) {
-//   const { attributes, listeners, setNodeRef } = useDraggable({
-//     id: id || 'draggable',
-//   });
-
-  // const x = transform? transform.x : 0;
-  // const y = transform?transform.y:0;
-
-//   const { x, y } = cordinates
-
-
-
-//   return (
-//     <div ref={setNodeRef}  {...listeners} {...attributes} style={{
-//       transform: `translate3d(${x}px, ${y}px, 0)`, width: "fit-content"
-//     }}>
-//       {children}
-//     </div>
-//   );
-// }
-
-
-// function Droppable({ children }: { children: React.ReactNode }) {
-//   const { setNodeRef } = useDroppable({
-//     id: 'droppable',
-//   });
-
-
-
-//   return (
-//     <div ref={setNodeRef} style={{ width: "100%" }}>
-//       {children}
-//     </div>
-//   );
-// }
